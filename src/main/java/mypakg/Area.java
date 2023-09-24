c4e8f8bfc3796a1e0b48d0b946306cd020b6e4a3
ddeshya <uddeshyasingh1111@gmail.com>
2023-09-24 12:02:49 -0700

    jenkins

---
+++ src/main/java/mypakg/Area.java
@@ -0,0 +1,30 @@
+package mypakg;
+
+import java.util.Scanner;
+
+public class Area {
+	public int square(int s) {
+		return s * s;
+	}
+
+	public int cuboid(int l, int b) {
+		return l * b;
+	}
+
+	public int volumeofcube(int a) {
+		return a * a * a;
+	}
+
+	public static void main(String[] args) {
+		 Scanner sc = new Scanner (System.in);
+		Area obj = new Area();
+		System.out.println("Enter the value of side of Square");
+		int num;
+		num =sc.nextInt();
+		System.out.println("Value is = " + obj.square(num));
+		System.out.println("Area 0f cuboid having value 8 and 9 = " + obj.cuboid(8, 9));
+		System.out.println("Volume is having value 8 and 6  = " + obj.volumeofcube(8));
+
+	}
+
+}
