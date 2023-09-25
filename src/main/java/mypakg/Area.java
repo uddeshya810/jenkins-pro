c468fea1e7a53ab3440b9762fdf072d90a8aa783
ddeshya <uddeshyasingh1111@gmail.com>
2023-09-25 00:45:34 -0700

    ndv

--- src/main/java/mypakg/Area.java
+++ src/main/java/mypakg/Area.java
@@ -17,6 +17,7 @@ public class Area {
 
 	public static void main(String[] args) {
 		 Scanner sc = new Scanner (System.in);
+		 System.out.println("Value is =   gbbfng");
 		Area obj = new Area();
 		System.out.println("Enter the value of side of Square");
 		int num;
@@ -24,7 +25,7 @@ public class Area {
 		System.out.println("Value is = " + obj.square(num));
 		System.out.println("Area 0f cuboid having value 8 and 9 = " + obj.cuboid(8, 9));
 		System.out.println("Volume is having value 8 and 6  = " + obj.volumeofcube(8));
-
+		System.out.println("Value is = " + obj.square(84));
 	}
 
 }
